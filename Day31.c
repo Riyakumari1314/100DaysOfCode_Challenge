bdf1d31e77e181f26d3e6c5489a67720243fed43
Riya Kumari <riyakumari198471@gmail.com>
2026-09-16 13:10:15 +0530

    Day32: Push and Pop in Stack

--- Day31.c
+++ Day31.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #define MAX 100
-
 int stack[MAX];
 int top = -1;
 
---
+++ Day32.c
@@ -0,0 +1,59 @@
+/*Day 32 - Question 1: Push and Pop in Stack
+Close
+Problem: Implement push and pop operations on a stack and verify stack operations.
+
+Input:
+- First line: integer n
+- Second line: n integers to push
+- Third line: integer m (number of pops)
+
+Output:
+- Print remaining stack elements from top to bottom
+
+Example:
+Input:
+5
+10 20 30 40 50
+2
+
+Output:
+30 20 10*/
+#include <stdio.h>
+
+#define MAX 100
+
+int main() {
+    int stack[MAX];
+    int n, m, top = -1;
+
+    // Number of elements to push
+    scanf("%d", &n);
+
+    // Push elements
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        stack[++top] = value;
+    }
+
+    // Number of pops
+    scanf("%d", &m);
+
+    // Pop elements
+    for (int i = 0; i < m; i++) {
+        if (top >= 0) {
+            top--;
+        }
+    }
+
+    // Display stack from top to bottom
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+
+        if (i > 0) {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
